dae49a74a69eb5c695de643c026aa9ad6353f600
MinghuiZou <96294755+MinghuiZou@users.noreply.github.com>
2022-03-02 18:54:14 +0800

    Update create_container.sh

--- dockerfile/create_container.sh
+++ dockerfile/create_container.sh
@@ -1,7 +1,8 @@
 docker create --gpus all -u user -h docker -it \
-              --name zmh_pt-1.8 common:pytorch-1.8 \
-              -v /home/user/zmh:/home/user/Programs
-              
+              -v /home/user/zmh:/home/user/Programs \
+              --name zmh_pt-1.8 common:pytorch-1.8
+
+
 # docker create --gpus all -u user -h docker -it \
-#               --name zmh_tf-0.12 common:tensorflow-0.12 \
-#               -v /home/user/zmh:/home/user/Programs
+#               -v /home/user/zmh:/home/user/Programs \
+#               --name zmh_tf-0.12 common:tensorflow-0.12
